53672aaaaeaea1ed42cdfc4ebb19309361def393
Vladislav <luckyvalenok1@gmail.com>
2021-05-24 16:27:15 +0500

    Необходима авторизация для доступа к некоторому контенту, добавление модели контента (серии и т.п)

--- CupOfSeries/settings.py
+++ CupOfSeries/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&swth7-js93z1_5jk4vec(ebaz!l5lfb7n9g_@6b%)579$&ye$'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- CupOfSeries/urls.py
+++ CupOfSeries/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('main.urls')),
                   path('accounts/', include('accounts.urls')),
                   path('accounts/', include('django.contrib.auth.urls')),
-                  path('', include('friendship.urls')),
-                  path('', include('chats.urls'))
+                  path('', login_required(include('friendship.urls'))),
+                  path('', login_required(include('chats.urls')))
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
