80d8e12d2e4ad319d099143df52667acf3ec026d
Vladislav <luckyvalenok1@gmail.com>
2021-05-24 16:30:36 +0500

    Исправление полей серий и сезонов

--- contents/models.py
+++ contents/models.py
@@ -5,12 +5,12 @@ from django.utils.translation import ugettext_lazy as _
 
 class Content(models.Model):
     name = models.TextField(_('Content name'), default='')
-    series = models.TextField(_('Content series'), default=1)
-    season = models.TextField(_('Content season'), default=1)
+    series = models.IntegerField(_('Content series'), default=1)
+    season = models.IntegerField(_('Content season'), default=1)
     pub_date = models.DateTimeField(_('Content date'), default=timezone.now)
     views = models.IntegerField(_('Content views'), default=0)
-    path = models.URLField(_('Content path'), default=0)
-    cover = models.URLField(_('Content background'), default=0)
+    path = models.URLField(_('Content path'))
+    cover = models.URLField(_('Content background'))
 
     def get_absolute_url(self):
         return '/view/%i/' % self.pk
